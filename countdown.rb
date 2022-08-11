f1c1f4d85184039447cc79e35d56caab423e2990
Sarah Mukuti <cesarwrites24@gmail.com>
2022-08-11 22:38:29 +0300

    commit tree 1cb0eb9711fb985e879f9b9791730c266936c9ba

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(number)
+    x = 0
+    while number > x
+        puts "#{number} SECOND(S)!"
+        number -= 1
+    end
+    "HAPPY NEW YEAR!"
 end
+
+countdown(10)
+
+def countdown_with_sleep(num_secs)
+    sleep(num_secs)
+end
+
+countdown_with_sleep(5)
